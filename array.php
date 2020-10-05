c7d0d62c4d62064866a793fc2d73f170869e792b
Aswini056 <72370248+Aswini056@users.noreply.github.com>
2020-10-05 10:58:08 +0530

    Add files via upload

---
+++ array.php
@@ -0,0 +1,32 @@
+<?Php
+
+$array = array(1, 2, 3, 4, 5);
+print_r($array);
+
+
+$array[] = 6;
+print_r($array);
+
+
+
+// Multidimensional Array
+
+$student = array (
+  array("Aswini",22,18),
+  array("Anusha",15,13),
+  array("Supraja",5,2),
+  array("Himaja",17,15)
+);
+    
+for ($row = 0; $row < 4; $row++) {
+  echo "<p><b>Row number $row</b></p>";
+  //echo "<ul>";
+  for ($col = 0; $col < 3; $col++) {
+    echo "<li>".$student[$row][$col]."</li>";
+  }
+  //echo "</ul>";
+}
+
+
+
+?>
